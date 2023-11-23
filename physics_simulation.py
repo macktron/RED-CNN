58cb78ad2732d582cebca37487ccf87a2f1f3aef
macktron <markusswegmark@hotmail.com>
2023-11-23 23:08:06 +0100

    Refactored code

--- physics_simulation.py
+++ physics_simulation.py
@@ -1,82 +1,105 @@
 import numpy as np
-from skimage.transform import radon, iradon
 import os
+from skimage.transform import radon, iradon
 import matplotlib.pyplot as plt
-import glob 
 
-
-def process_images(path, N0 = 10**10, mu_water=0.2,mu_air=0, plot=False, save=False, save_dir = "npy_img_physics_simulation"):
-    
+def get_image_names(path):
     if path.endswith('.npy'):
-        print('Path is a file, load directory instead')
-        exit()
+        return [path]
+    if not os.path.exists(path):
+        raise ValueError('Provided directory or file does not exist.')
+    return [file for file in os.listdir(path) if file.endswith('target.npy')]
 
-    if not os.path.exists(save_to_dir):
-        print('Directory does not exist')
-        exit()
+def process_image(image_path, N0 = 10**8, mu_water = 0.2, mu_air = 0.0, pixel_size_cm = 0.0264583333):
 
-    if not os.path.exists(source_dir):
-        print('Directory does not exist')
-        exit()
+    # Load image
+    image = np.load(image_path)
 
-    image_names = [file for file in os.listdir(path) if file.endswith('target.npy')]
-    processed_images = []
+    # Beräkna radontransformen
+    theta = np.linspace(0., 180., max(image.shape), endpoint=False)
+    sinogram = radon(image, theta=theta, circle=False)
 
-    for image_name in image_names:
-        image = np.load(os.path.join(path, image_name))
-        # Beräkna radontransformen
-        theta = np.linspace(0., 180., max(image.shape), endpoint=False)
-        sinogram = radon(image, theta=theta, circle=True)
+    # Skala om för att få linjeintegralen dimensionslös
+    sinogram *= pixel_size_cm
 
-        # Skala om för att få linjeintegralen dimensionslös
-        pixel_size = 0.2645833333
-        sinogram *= pixel_size
+    # Beräkna antalet fotoner som har passerat genom objektet
+    lambda_values = N0 * np.exp(-sinogram)
 
-        # Beräkna antalet fotoner som har passerat genom objektet
-        lambda_values = N0 * np.exp(-sinogram)
-        
-        N = np.random.poisson(lambda_values)
+    # Simulera poissonfördelad slump
+    N = np.random.poisson(lambda_values)
+    N = np.maximum(N, 1)  # Filter out values that are too low
 
-        N = np.maximum(N, 1) # Filter out bad values
+    # Beräkna linjeintegralen
+    p = -np.log(N / N0)
 
-        p = -np.log(N / N0)
-    
+    # Invertera radontransformen
+    reconstruction = iradon(p, theta=theta, circle=False)
 
-        # Invertera radontransformen
-        reconstruction = iradon(p, theta=theta, circle=True)
+    # Skala om
+    reconstruction *= pixel_size_cm
+    return reconstruction
+    simulated_images = 1000 * (reconstruction - mu_water) / (mu_water - mu_air)
 
-        # Skala om
-        reconstruction *= pixel_size
-        CT = 1000 * (reconstruction - mu_water) / (mu_water - mu_air)
+    # Rescale to 0,1
+    simulated_images -= np.min(simulated_images)
+    simulated_images /= np.max(simulated_images)
+    simulated_images *= np.max(image)
+    return simulated_images
 
-        if save: 
-            np.save(os.path.join(save_dir, image_name), CT)
-            plt.imsave(os.path.join(save_dir, image_name.replace(".npy", ".pdf")), CT, cmap=plt.cm.Greys_r)
+def plot_images(original_image, CT_image, save_path=None):
+    fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+    ax = axes[0]
+    ax.imshow(original_image, cmap=plt.cm.Greys_r)
+    ax.set_title('Original Image')
+    ax.axis('off')
+    ax = axes[1]
+    ax.imshow(CT_image, cmap=plt.cm.Greys_r)
+    ax.set_title('Reconstructed CT Image')
+    ax.axis('off')
+    if save_path:
+        plt.savefig(save_path)
+    plt.show()
 
-        if plot:
-            fig, axes = plt.subplots(1, 2, figsize=(8, 4))  # Adjusted to 1 row, 2 columns
+def save_images(CT_image, save_dir, image_name):
+    np.save(os.path.join(save_dir, image_name), CT_image)
+    plt.imsave(os.path.join(save_dir, image_name.replace(".npy", ".pdf")), CT_image, cmap=plt.cm.Greys_r)
 
-            # Original Image
-            ax = axes[0]
-            ax.imshow(image, cmap=plt.cm.Greys_r)
-            ax.set_title('Original Image')
-            ax.axis('off')
+def get_noise(original_image, CT_image):
 
-            # Reconstructed CT Image
-            ax = axes[1]
-            ax.imshow(CT, cmap=plt.cm.Greys_r)
-            ax.set_title('Reconstructed CT Image')
-            ax.axis('off')
+    theta = np.linspace(0., 180., max(image.shape), endpoint=False)
+    sinogram = radon(original_image, theta=theta, circle=False)
+    original_reconstructed = iradon(sinogram, theta=theta, circle=False)
+    variance_original = np.std(original_reconstructed)**2
+    variance_CT = np.std(CT_image)**2
 
-            plt.show()
+    print(f"Original max: {np.max(original_image)} min: {np.min(original_image)}")
+    print(f"CT max: {np.max(CT_image)} min: {np.min(CT_image)}")
 
+    print(f"Dose: {100*variance_original / variance_CT}%")
 
 
-# Parameters
 source_dir = 'npy_img/'  # Directory with original files
-save = True
-save_to_dir = 'npy_img_physics_simulation/'  # Directory to save modified files
-N0 = 10**15 # Antal fotoner som träffar detektorerna i tomma luften
 
+N0 = 10**5
+mu_water = 0.2
+mu_air = 0
+save = False
+plot = True
 
-processed_images = process_images(source_dir, plot=False, N0=N0, mu_water=0.2, mu_air=0,save=save, save_dir=save_to_dir)
+save_dir = "npy_img_physics_simulation"
+save_dir_pdf = "pdf_img_physics_simulation"
+
+image_names = get_image_names(source_dir)
+
+for image_name in image_names:
+    
+    image_path = os.path.join(source_dir, image_name)
+    image = np.load(image_path)
+
+    CT = process_image(image_path, N0, mu_water, mu_air)
+    get_noise(image, CT)
+
+    if save:
+        save_images(CT, save_dir, image_name)
+    if plot:
+        plot_images(np.load(image_path), CT, save_path=os.path.join(save_dir_pdf, image_name.replace(".npy", ".pdf")) if save else None)
