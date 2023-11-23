d852bbd9c6250fde00666eaa1be8aae878ccb285
macktron <markusswegmark@hotmail.com>
2023-11-23 16:29:58 +0100

    Kind of works

--- physics_simulation.py
+++ physics_simulation.py
@@ -2,68 +2,87 @@ import numpy as np
 from skimage.transform import radon, iradon
 import os
 import matplotlib.pyplot as plt
+import glob 
 
-def process_images(path):
 
-    #Ladda bilderna
+def process_images(path, N0 = 10**10, mu_water=0.2,mu_air=0, plot=False, save=False, save_dir = "npy_img_physics_simulation"):
+    
     if path.endswith('.npy'):
-        images = [np.load(path)]
-    else:
-        images = [np.load(os.path.join(path, file)) for file in os.listdir(path) if file.endswith('.npy')]
+        print('Path is a file, load directory instead')
+        exit()
 
-    # Konstanten för luftens µ-värde
-    mu_air = 0
+    if not os.path.exists(save_to_dir):
+        print('Directory does not exist')
+        exit()
 
+    if not os.path.exists(source_dir):
+        print('Directory does not exist')
+        exit()
+
+    images = [np.load(os.path.join(path, file)) for file in os.listdir(path) if file.endswith('.npy')]
     processed_images = []
 
     for image in images:
-        
         # Beräkna radontransformen
         theta = np.linspace(0., 180., max(image.shape), endpoint=False)
         sinogram = radon(image, theta=theta, circle=True)
 
-        
         # Skala om för att få linjeintegralen dimensionslös
-        pixel_size = 0.2645833333  
+        pixel_size = 0.2645833333
         sinogram *= pixel_size
 
-
-        N0 = 1 # Hur bestämmer vi detta?
-
-        # Osäker om detta är rätt
+        # Beräkna antalet fotoner som har passerat genom objektet
         lambda_values = N0 * np.exp(-sinogram)
         N = np.random.poisson(lambda_values)
-        p = -np.log(N / N0)
 
+        p = -np.log(N / N0 + 1/10**11 )  # Lägg till  liten konstant för att undvika log med noll
+        
+        
         # Invertera radontransformen
         reconstruction = iradon(p, theta=theta, circle=True)
 
-        # Skala om tillbaka till pixlar
-        mu_water = 0.2 # µ-värde för vatten
+        # Skala om
         reconstruction *= pixel_size
         CT = 1000 * (reconstruction - mu_water) / (mu_water - mu_air)
 
-        # Visa bilderna
-        plt.figure(figsize=(15, 5))
-        plt.subplot(1, 3, 1)
-        plt.imshow(image, cmap=plt.cm.Greys_r)
-        plt.title('Originalbild')
-        plt.axis('off')
+        if plot:
+            fig, axes = plt.subplots(1, 2, figsize=(8, 4))  # Adjusted to 1 row, 2 columns
 
-        plt.subplot(1, 3, 2)
-        plt.imshow(sinogram, cmap=plt.cm.Greys_r)
-        plt.title('Ursprungligt Sinogram')
-        plt.axis('off')
+            # Original Image
+            ax = axes[0]
+            ax.imshow(image, cmap=plt.cm.Greys_r)
+            ax.set_title('Original Image')
+            ax.axis('off')
 
-        plt.subplot(1, 3, 3)
-        plt.imshow(CT, cmap=plt.cm.Greys_r)
-        plt.title('Rekonstruerad Bild')
-        plt.axis('off')
+            # Reconstructed CT Image
+            ax = axes[1]
+            ax.imshow(CT, cmap=plt.cm.Greys_r)
+            ax.set_title('Reconstructed CT Image')
+            ax.axis('off')
 
-        plt.show()
+            plt.show()
 
-    return processed_images
+        if save: 
+            for i, input_file_path in enumerate(glob.glob(os.path.join(path, '*input.npy'))):
+                input_file_base_name = os.path.basename(input_file_path)
+                base_name = input_file_base_name.replace('_input.npy', '')
+                target_file_base_name = base_name + '_target.npy'
+                target_file_path = os.path.join(path, target_file_base_name)
 
-path = "./npy_img/L506_0_input.npy"  # Ersätt med faktisk sökväg
-processed_images = process_images(path)
+                result_input_path = os.path.join(save_to_dir, input_file_base_name)
+                result_target_path = os.path.join(save_to_dir, target_file_base_name)
+
+                np.save(result_input_path, processed_images[i])
+
+
+
+
+# Parameters
+source_dir = 'npy_img/'  # Directory with original files
+save_to_dir = 'npy_img_physics_simulation'  # Directory to save modified files
+N0 = 10**12 # Antal fotoner som träffar detektorerna i tomma luften
+
+
+processed_images = process_images(source_dir, plot=True, N0=N0, mu_water=0.2, mu_air=0,save=False, save_dir=save_to_dir)
+
 
